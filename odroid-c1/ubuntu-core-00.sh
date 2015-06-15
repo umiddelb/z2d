d313eb7c7ba2d4983a91f61dcf971a41ec16cde4
Uli Middelberg <uli@middelberg.de>
2015-06-15 21:27:41 +0200

    Update ubuntu-core-00.sh

--- odroid-c1/ubuntu-core-00.sh
+++ odroid-c1/ubuntu-core-00.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 dev=sda
-curl -sSL https://raw.githubusercontent.com/mdrjr/c1_uboot_binaries/master/bl1.bin.hardkernel | sudo dd of=/dev/$dev bs=1 count=442
-curl -sSL https://raw.githubusercontent.com/mdrjr/c1_uboot_binaries/master/bl1.bin.hardkernel | sudo dd of=/dev/$dev bs=512 skip=1 seek=1
-curl -sSL https://raw.githubusercontent.com/mdrjr/c1_uboot_binaries/master/u-boot.bin | sudo dd of=/dev/$dev bs=512 seek=64
+curl -sSL https://raw.githubusercontent.com/mdrjr/c1_uboot_binaries/master/bl1.bin.hardkernel > /tmp/bl1.bin.hardkernel
+curl -sSL https://raw.githubusercontent.com/mdrjr/c1_uboot_binaries/master/u-boot.bin > /tmp/u-boot.bin
+
+sudo dd if=/tmp/bl1.bin.hardkernel of=/dev/$dev bs=1 count=442
+sudo dd if=/tmp/bl1.bin.hardkernel of=/dev/$dev bs=512 skip=1 seek=1
+sudo dd if=/tmp/u-boot.bin of=/dev/$dev bs=512 seek=64
+rm -f /tmp/bl1.bin.hardkernel /tmp/u-boot.bin
 sync
 /bin/echo -e "o\nn\np\n1\n3072\n262143\nn\np\n2\n262144\n\nt\n1\nb\nw\n" | sudo fdisk /dev/$dev
 sync
