7c6261f25731d21690616b443f3e8114d1f0a4b3
Uli Middelberg <uli@middelberg.de>
2015-11-10 11:21:09 +0100

    Update ubuntu-docker-00.sh

--- common/ubuntu-docker-00.sh
+++ common/ubuntu-docker-00.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get -y install lxc aufs-tools cgroup-lite apparmor docker.io
 sudo service docker stop
-sudo curl -sSL https://github.com/umiddelb/armhf/blob/master/bin/docker-1.9.0?raw=true >/usr/bin/docker
+curl -sSL https://github.com/umiddelb/armhf/blob/master/bin/docker-1.9.0?raw=true | sudo dd of=/usr/bin/docker
 sudo service docker start
 sudo usermod -aG docker $SUDO_USER
