3f6b0394b27508a2110674868eb7cd3308671145
Uli Middelberg <uli@middelberg.de>
2015-12-03 22:25:22 +0100

    tidy up and initial release for utilite pro

--- utilite-pro/ubuntu-core-02.sh
+++ utilite-pro/ubuntu-core-02.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 c_locale "en_GB.UTF-8"
 c_tzone "Europe/Berlin"
-c_hostname "cbxi"
+c_hostname "upro"
 c_apt_list "trusty"
 c_nameserver "8.8.8.8"
 
@@ -15,10 +15,10 @@ r_pkg_upgrade
 i_base
 i_extra
 i_gcc
-i_kernel_cubox_i
+i_kernel_utilite_pro
 c_if_lo
 c_if_dhcp "eth0"
-c_ttyS "ttymxc0"
+c_ttyS "ttymxc3"
 c_user "ubuntu"
 
 apt-get clean
