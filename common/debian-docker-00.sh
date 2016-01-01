e6d41147bc719da97f02f05ee245aab9d9f4bb6d
Uli Middelberg <uli@middelberg.de>
2016-01-01 21:56:41 +0100

    Update debian-docker-00.sh

--- common/debian-docker-00.sh
+++ common/debian-docker-00.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get -y install lxc aufs-tools cgroupfs-mount cgroup-bin apparmor docker.io
+sudo apt-get -y install lxc aufs-tools cgroupfs-mount cgroup-bin apparmor docker.io/jessie-backports
 sudo service docker stop
 curl -sSL https://github.com/umiddelb/armhf/blob/master/bin/docker-1.9.0?raw=true | sudo dd of=/usr/bin/docker
 sudo service docker start
