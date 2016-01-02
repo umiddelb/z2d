073c47d9b5e3f5eb80ad06328fe5e874497cc5ca
Uli Middelberg <uli@middelberg.de>
2016-01-02 21:50:26 +0100

    aarch64/arm64 support for pine64 ubuntu/debian

---
+++ pine64/debian-02.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+. ./common-functions.sh
+
+debootstrap/debootstrap --second-stage
+
+c_locale_debian "en_GB.UTF-8"
+c_tzone "Europe/Berlin"
+c_hostname "p64"
+c_apt_list_debian "jessie"
+c_nameserver "8.8.8.8"
+
+dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
+export DEBIAN_FRONTEND=noninteractive
+
+echo 'APT::Cache-Limit "500000000";' >> /etc/apt/apt.conf.d/70debconf
+echo 'APT::Cache-Start "100000000";' >> /etc/apt/apt.conf.d/70debconf
+
+r_pkg_upgrade
+i_base_debian
+
+# curl -sSL http://ftp.debian-ports.org/archive/archive_2015.key | apt-key add -
+# echo 'deb http://ftp.debian-ports.org/debian/ unstable main' >> /etc/apt/sources.list.d/ports.list
+# echo 'deb http://ftp.debian-ports.org/debian/ unreleased main' >> /etc/apt/sources.list.d/ports.list
+
+apt-get -q=2 -y install screen wireless-tools iw libncurses5-dev cpufrequtils rcs aptitude make bc man-db ntp usbutils pciutils lsof most sysfsutils
+i_gcc_debian
+
+c_if_lo
+c_if_dhcp "eth0"
+c_user "debian"
+
+apt-get clean
+
+rm /sbin/initctl; dpkg-divert --local --rename --remove /sbin/initctl
--- pine64/ubuntu-02.sh
+++ pine64/ubuntu-02.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 . ./common-functions.sh
 
+debootstrap/debootstrap --second-stage
+
 export DEBIAN_FRONTEND=noninteractive
 
 c_locale "en_GB.UTF-8"
 c_tzone "Europe/Berlin"
-c_hostname "cbxi"
+c_hostname "p64"
 c_apt_list "trusty"
 c_nameserver "8.8.8.8"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 
-r_pkg_upgrade
+echo 'APT::Cache-Limit "500000000";' >> /etc/apt/apt.conf.d/70debconf
+echo 'APT::Cache-Start "100000000";' >> /etc/apt/apt.conf.d/70debconf
+apt-get -q=2 update
 i_base
 i_extra
 i_gcc
-i_kernel_cubox_i
 c_if_lo
 c_if_dhcp "eth0"
-c_ttyS "ttymxc0"
-c_fw_utils "/dev/mmcblk0 0x60000 0x2000 0x2000"
 c_user "ubuntu"
 
 apt-get clean
