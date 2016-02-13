b27adc971326e36638788552b808a67534ba0f89
Uli Middelberg <uli@middelberg.de>
2016-02-14 00:36:45 +0100

    finished Debian

--- odroid-c2/debian-01.sh
+++ odroid-c2/debian-01.sh
@@ -11,7 +11,7 @@ sudo mount -o bind /proc ./rootfs/proc
 sudo cp /proc/mounts ./rootfs/etc/mtab
 sudo cp debian-02.sh ./rootfs
 sudo cp common-functions.sh ./rootfs
-# sudo cp debian-docker-00.sh ./rootfs
+sudo cp debian-docker-00.sh ./rootfs
 
 sudo LC_ALL=C LANGUAGE=C LANG=C chroot ./rootfs bash /debian-02.sh
 sudo rm ./rootfs/debian-02.sh
