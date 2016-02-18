77568e5271c39aa5f8e17b7123fed58eab2d7394
Uli Middelberg <uli@middelberg.de>
2016-02-18 23:07:54 +0100

    PINE64: ubuntu support

--- pine64/ubuntu-02.sh
+++ pine64/ubuntu-02.sh
@@ -13,14 +13,14 @@ c_nameserver "8.8.8.8"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 
-echo 'APT::Cache-Limit "500000000";' >> /etc/apt/apt.conf.d/70debconf
-echo 'APT::Cache-Start "100000000";' >> /etc/apt/apt.conf.d/70debconf
 apt-get -q=2 update
 i_base
 i_extra
 i_gcc
+i_kernel_pine64
 c_if_lo
 c_if_dhcp "eth0"
+c_ttyS "ttyS0"
 c_user "ubuntu"
 
 apt-get clean
