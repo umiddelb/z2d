edeb71fea1837dd77af0d71f902936895c218914
Uli Middelberg <uli@middelberg.de>
2016-02-22 21:37:40 +0100

    PINE64: update

--- pine64/debian-00.sh
+++ pine64/debian-00.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -ex
 
-dev=sdb
-curl -sSL https://www.stdin.xyz/downloads/people/longsleep/tmp/pine64-images/simpleimage-pine64-20160207-1.img.xz | unxz | sudo dd of=/dev/$dev
+dev=sdc
+curl -sSL https://www.stdin.xyz/downloads/people/longsleep/tmp/pine64-images/simpleimage-pine64-20160221-4.img.xz | unxz | sudo dd of=/dev/$dev
 sync
 /bin/echo -e "d\n2\nn\np\n2\n143360\n\nw\n" | sudo fdisk /dev/$dev
 sync
 sudo mkfs.ext4 -O ^has_journal -b 4096 -L rootfs -U deadbeef-dead-beef-dead-beefdeadbeef /dev/${dev}2 
 sudo mount /dev/${dev}2 ./rootfs
+sudo mkdir ./rootfs/boot
+sudo mount /dev/${dev}1 ./rootfs/boot
