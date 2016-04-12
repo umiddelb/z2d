2d05f9942af887d455e5706ac06b9a9b085679f4
Uli Middelberg <uli@middelberg.de>
2016-04-12 17:15:22 +0200

    PINE64:update

--- common/common-functions.sh
+++ common/common-functions.sh
@@ -168,7 +168,7 @@ i_kernel_cubox_i () {
 i_kernel_pine64 () {
   curl -sSL https://www.stdin.xyz/downloads/people/longsleep/pine64-images/linux/linux-pine64-latest.tar.xz | sudo tar --numeric-owner -xJvf -
   mkdir -p /boot/conf.d/system.default
-  curl -sSL https://raw.githubusercontent.com/umiddelb/u-571/master/board/pine64/uEnv.txt > /boot/conf.d/system.default/uEnv.txt
+  curl -sSL https://raw.githubusercontent.com/umiddelb/u-571/master/board/pine64+/uEnv.txt > /boot/conf.d/system.default/uEnv.txt
   (cd /boot/conf.d/ ; ln -s system.default default)
   (cd /boot/conf.d/system.default; ln -s /boot/pine64 kernel; ln -s /boot/initrd.img Initrd)
 }
--- pine64/debian-00.sh
+++ pine64/debian-00.sh
@@ -4,10 +4,13 @@ set -ex
 dev=sdb
 curl -sSL https://www.stdin.xyz/downloads/people/longsleep/pine64-images/simpleimage-pine64-latest.img.xz | unxz | sudo dd of=/dev/$dev
 sync
+set +e
 /bin/echo -e "d\n2\nn\np\n2\n143360\n\nw\n" | sudo fdisk /dev/$dev
+set -e
 sync
 sudo mkfs.ext4 -O ^has_journal -b 4096 -L rootfs -U deadbeef-dead-beef-dead-beefdeadbeef /dev/${dev}2 
 sudo mount /dev/${dev}2 ./rootfs
 sudo mkdir ./rootfs/bootenv
 sudo mount /dev/${dev}1 ./rootfs/bootenv
-curl -sSL https://raw.githubusercontent.com/umiddelb/u-571/master/board/pine64/bundle.uEnv > ./rootfs/bootenv/bundle.uEnv
+sudo rm -rf rootfs/bootenv/initrd.img rootfs/bootenv/pine64/
+curl -sSL https://raw.githubusercontent.com/umiddelb/u-571/master/board/pine64+/bundle.uEnv | sudo dd of=./rootfs/bootenv/bundle.uEnv
