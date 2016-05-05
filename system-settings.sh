c2e52460a6c6101fda0825116a50f8945e24d658
Nerten <nerten@macpaw.com>
2016-05-05 23:56:02 +0300

    starting to implement idea with system settings config which described here: http://forum.odroid.com/viewtopic.php?p=91096#p91096

---
+++ system-settings.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Enter a space-separated list of valid locales, e.g. "en_US.UTF-8 en_GB.UTF-8".
+LOCALES="en_GB.UTF-8 de_DE.UTF-8"
+
+# Enter the timezone for your system, e.g. "Europe/Berlin".
+TIMEZONE="Europe/Berlin"
+
+# Enter a space-separated list of nameservers, e.g. "8.8.8.8 8.8.4.4"
+NAMESERVERS="8.8.8.8 8.8.4.4"
+
+# Enter the username for your system.
+USERNAME="username"
+
+# Enter the hostname for your CuBox-i system.
+CBXI_HOSTNAME="cbxi"
+
+# Enter the hostname for your Odroid-C1 system.
+C1_HOSTNAME="c1"
+
+# Enter the hostname for your Odroid-C2 system.
+C2_HOSTNAME="c2"
+
+# Enter the hostname for your Odroid-XU4 system.
+XU4_HOSTNAME="xu4"
+
+# Enter the hostname for your PINE64 system.
+P64_HOSTNAME="p64"
+
+# Enter the hostname for your Utilite Pro system.
+UPRO_HOSTNAME="upro"
+
+
+
+
