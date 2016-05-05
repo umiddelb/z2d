d5842af6a81366db9502d63f83bded7378981aa7
Nerten <nerten@macpaw.com>
2016-05-06 00:18:11 +0300

    preparing to use system-settings.sh in common scripts

--- common/debian-01.sh
+++ common/debian-01.sh
@@ -12,11 +12,13 @@ sudo mount -o bind /proc ./rootfs/proc
 sudo cp /proc/mounts ./rootfs/etc/mtab
 sudo cp debian-02.sh ./rootfs
 sudo cp common-functions.sh ./rootfs
+sudo cp system-settings.sh ./rootfs
 sudo cp debian-docker-00.sh ./rootfs
 
 sudo LC_ALL=C LANGUAGE=C LANG=C chroot ./rootfs bash /debian-02.sh
 sudo rm ./rootfs/debian-02.sh
 sudo rm ./rootfs/common-functions.sh
+sudo rm ./rootfs/system-settings.sh
 
 set +e
 sudo umount -lf ./rootfs/dev/pts
--- common/ubuntu64-core-01.sh
+++ common/ubuntu64-core-01.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 set -ex
 
 curl -sSL http://cdimage.ubuntu.com/ubuntu-core/releases/15.10/release/ubuntu-core-15.10-core-arm64.tar.gz | sudo tar --numeric-owner -xpzf - -C rootfs/
@@ -11,11 +10,13 @@ sudo mount -t proc /proc ./rootfs/proc
 sudo cp /proc/mounts ./rootfs/etc/mtab
 sudo cp ubuntu-core-02.sh ./rootfs
 sudo cp common-functions.sh ./rootfs
+sudo cp system-settings.sh ./rootfs
 sudo cp ubuntu-docker-00.sh ./rootfs
 
 sudo chroot ./rootfs bash /ubuntu-core-02.sh
 sudo rm ./rootfs/ubuntu-core-02.sh
 sudo rm ./rootfs/common-functions.sh
+sudo rm ./rootfs/system-settings.sh
 
 sudo umount ./rootfs/dev/pts
 sudo umount ./rootfs/sys
