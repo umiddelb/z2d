c424fda764a4df5e58aa3c2a490f64c1eeafd142
Nerten <nerten@macpaw.com>
2016-05-06 16:01:00 +0300

    using system-settings.sh for cubox-i

--- cubox-i/debian-02.sh
+++ cubox-i/debian-02.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 . ./common-functions.sh
+. ./system-settings.sh
 
 debootstrap/debootstrap --second-stage
 
-c_locale_debian "en_GB.UTF-8 de_DE.UTF-8"
-c_tzone "Europe/Berlin"
-c_hostname "cbxi"
+c_locale_debian $LOCALES
+c_tzone $TIMEZONE
+c_hostname $CBXI_HOSTNAME
 c_apt_list_debian "jessie"
-c_nameserver "8.8.8.8"
+c_nameserver $NAMESERVERS
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 export DEBIAN_FRONTEND=noninteractive
@@ -23,7 +24,7 @@ c_if_lo
 c_if_dhcp "eth0"
 c_ttyS_debian "ttymxc0"
 c_fw_utils "/dev/mmcblk0 0x60000 0x2000 0x2000"
-c_user "debian"
+c_user $USERNAME
 
 apt-get clean
 
