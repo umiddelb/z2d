ce54e2afce88f51546b1042d8e86a9bb3e23f478
Nerten <nerten@macpaw.com>
2016-05-06 16:23:23 +0300

    using system-settings.sh for utilite pro

--- utilite-pro/ubuntu-core-02.sh
+++ utilite-pro/ubuntu-core-02.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 . ./common-functions.sh
+. ./system-settings.sh
 
 export DEBIAN_FRONTEND=noninteractive
 
-c_locale "en_GB.UTF-8 de_DE.UTF-8"
-c_tzone "Europe/Berlin"
-c_hostname "upro"
-c_apt_list "trusty"
-c_nameserver "8.8.8.8"
+c_locale $LOCALES
+c_tzone $TIMEZONE
+c_hostname $UPRO_HOSTNAME
+c_apt_list "xenial"
+c_nameserver $NAMESERVERS
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 
@@ -20,7 +21,7 @@ c_if_lo
 c_if_dhcp "eth0"
 c_ttyS "ttymxc3"
 c_fw_utils "/dev/mtd1 0xc0000 0x2000 0x2000"
-c_user "ubuntu"
+c_user $USERNAME
 
 apt-get clean
 
