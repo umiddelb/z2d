bbfa717f7e1b2d083bc4694ab0b114a220f93be2
Uli Middelberg <uli@middelberg.de>
2017-01-07 22:18:43 +0100

    C2: harmonization to armhf

--- odroid-c2/centos-03.sh
+++ odroid-c2/centos-03.sh
@@ -3,13 +3,17 @@ set -ex
 
 . ./common-functions.sh
 
+yum install -y ntp ntpdate
+chkconfig ntpd on
+ntpdate pool.ntp.org
+
 timedatectl set-timezone Europe/Berlin
 localectl set-locale LANG=en_GB.UTF-8
 hostnamectl set-hostname c2 --static
 hostnamectl set-hostname "ODROID-C2" --pretty
 
 yum update -y
-yum install -y ntp bc bridge-utils docker dtc iw lzop rcs screen sysfsutils usbutils pciutils wget lsof ntfs-3g
+yum install -y bc bridge-utils docker dtc iw lzop rcs screen sysfsutils usbutils pciutils wget lsof ntfs-3g
 c_yum_list_epel_aarch64
 # yum install -y gcc --enablerepo=warning:fedora23
 # yum install -y most uboot-tools --enablerepo=warning:fedora24
