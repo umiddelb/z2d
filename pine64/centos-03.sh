96e33f29fd782b9f2bf49ac3dfbbc3fb42760b43
Uli Middelberg <uli@middelberg.de>
2017-04-10 22:50:21 +0200

    P64: remove f* repo usage

--- pine64/centos-03.sh
+++ pine64/centos-03.sh
@@ -3,17 +3,20 @@ set -ex
 
 . ./common-functions.sh
 
+yum install -y ntp ntpdate
+chkconfig ntpd on
+ntpdate pool.ntp.org
+
 timedatectl set-timezone Europe/Berlin
 localectl set-locale LANG=en_GB.UTF-8
 hostnamectl set-hostname p64 --static
 hostnamectl set-hostname "PINE64+" --pretty
 
 yum update -y
-yum install -y bc bridge-utils docker dtc iw lzop rcs screen sysfsutils usbutils wget
-c_yum_list_f23_second
-yum install -y gcc --enablerepo=warning:fedora23
-c_yum_list_f24_second
-yum install -y most ntfs-3g uboot-tools --enablerepo=warning:fedora24
+yum install -y bc bridge-utils docker dtc iw lzop rcs screen sysfsutils usbutils pciutils wget lsof ntfs-3g
+c_yum_list_epel_aarch64
+# c_yum_list_f25_second
+# yum install -y most uboot-tools --enablerepo=warning:fedora24
 yum clean all
 
 c_fw_utils "/dev/mmcblk0 0x88000 0x20000"
