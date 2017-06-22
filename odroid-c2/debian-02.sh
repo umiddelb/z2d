2083d5f0f3e08f0f095147450319f1fe0a40eb91
Uli Middelberg <uli@middelberg.de>
2017-06-22 13:12:58 +0000

    Debian stretch: gcc-6 is now the default compiler

--- odroid-c2/debian-02.sh
+++ odroid-c2/debian-02.sh
@@ -6,7 +6,7 @@ debootstrap/debootstrap --second-stage
 c_locale_debian "en_GB.UTF-8" "de_DE.UTF-8"
 c_tzone "Europe/Berlin"
 c_hostname "c2"
-c_apt_list_debian "jessie"
+c_apt_list_debian "stretch"
 c_nameserver "8.8.8.8"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
@@ -16,9 +16,8 @@ r_pkg_upgrade
 i_base_debian
 
 # i_extra_debian
-apt-get -q=2 -y install screen wireless-tools iw libncurses5-dev cpufrequtils rcs aptitude make bc man-db ntp usbutils pciutils lsof most sysfsutils
+apt-get -q=2 -y install screen wireless-tools iw libncurses5-dev cpufrequtils rcs aptitude gcc make bc man-db ntp usbutils pciutils lsof most sysfsutils
 
-i_gcc_debian
 i_kernel_odroid_c2_412
 
 c_if_lo
