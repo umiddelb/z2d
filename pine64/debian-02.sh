bfd63411f89e026f73d60c57a2883581515f1d21
Uli Middelberg <uli@middelberg.de>
2017-07-05 14:41:18 +0200

    Pine64: update to Debian 9

--- pine64/debian-02.sh
+++ pine64/debian-02.sh
@@ -7,7 +7,7 @@ debootstrap/debootstrap --second-stage
 c_locale_debian "en_GB.UTF-8" "de_DE.UTF-8"
 c_tzone "Europe/Berlin"
 c_hostname "p64"
-c_apt_list_debian "jessie"
+c_apt_list_debian "stretch"
 c_nameserver "8.8.8.8"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
@@ -16,12 +16,8 @@ export DEBIAN_FRONTEND=noninteractive
 r_pkg_upgrade
 i_base_debian
 
-# curl -sSL http://ftp.debian-ports.org/archive/archive_2015.key | apt-key add -
-# echo 'deb http://ftp.debian-ports.org/debian/ unstable main' >> /etc/apt/sources.list.d/ports.list
-# echo 'deb http://ftp.debian-ports.org/debian/ unreleased main' >> /etc/apt/sources.list.d/ports.list
+apt-get -q=2 -y install dialog screen wireless-tools iw libncurses5-dev cpufrequtils rcs aptitude gcc make bc lzop man-db ntp usbutils pciutils lsof most sysfsutils
 
-apt-get -q=2 -y install screen wireless-tools iw libncurses5-dev cpufrequtils rcs aptitude make bc man-db ntp usbutils pciutils lsof most sysfsutils
-i_gcc_debian
 i_kernel_pine64
 
 c_if_lo
