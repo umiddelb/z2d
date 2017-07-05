3d629d3c6a7c0ec340090e0c9c13c84fa0c7afab
Uli Middelberg <uli@middelberg.de>
2017-07-05 21:40:30 +0200

    XU4: CentOS update

--- odroid-xu4/centos-02.sh
+++ odroid-xu4/centos-02.sh
@@ -5,7 +5,6 @@ set -ex
 
 c_nameserver 8.8.8.8
 
-mv /etc/fstab /etc/fstab.rpmdefault
 echo "UUID=deadbeef-dead-beef-dead-beefdeadbeef /                       ext4     defaults        0 0" > /etc/fstab
 
 i_kernel_odroid_xu4_490
@@ -14,3 +13,5 @@ passwd root
 adduser -c '' centos
 usermod -aG adm,cdrom,wheel,dialout centos
 passwd centos
+
+touch /etc/sysconfig/network
