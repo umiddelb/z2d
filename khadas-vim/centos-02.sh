fbf67532b1502a4aedd5250d5aee2c26143937fc
Uli Middelberg <uli@midelberg.de>
2017-07-05 05:01:14 +0000

    KVIM: Basic CentOS support

---
+++ khadas-vim/centos-02.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -ex
+
+. ./common-functions.sh
+
+c_nameserver 8.8.8.8
+
+echo "UUID=deadbeef-dead-beef-dead-beefdeadbeef /                       ext4     defaults        0 0" > /etc/fstab
+
+i_kernel_odroid_kvim_31429
+
+passwd root
+adduser -c '' centos
+usermod -aG adm,cdrom,wheel,dialout centos
+passwd centos
+
+touch /etc/sysconfig/network
---
+++ khadas-vim/centos-03.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+
+. ./common-functions.sh
+
+yum install -y ntp ntpdate
+chkconfig ntpd on
+ntpdate pool.ntp.org
+
+timedatectl set-timezone Europe/Berlin
+localectl set-locale LANG=en_GB.UTF-8
+hostnamectl set-hostname kvim --static
+hostnamectl set-hostname "Khadas VIM" --pretty
+
+c_yum_list_epel_aarch64
+c_yum_list_f25_second
+i_base_centos
+
+c_docker_centos
+
+c_fw_utils "/dev/mmcblk0 0x7400000 0x10000"
