930fd03332f11edc6c63a06359e6efa60315ec99
Uli Middelberg <uli@middelberg.de>
2017-12-05 22:51:28 +0100

    update

--- common/setrootfs.sh
+++ common/setrootfs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-uuid=`sudo blkid $1 | cut -f 2 -d ' ' | tr -d '"'`
+uuid=`sudo blkid $1 | tr ' ' '\n' | grep ^UUID= | tr -d '"'`
 sudo sed -i -e "s/UUID=\\w*-\\w*-\\w*-\\w*-\\w*/$uuid/" ./rootfs/boot/conf.d/default/uEnv.txt 
