a534f283ac9053d893eb1e20624bb9c7f9eec048
Uli Middelberg <uli@middelberg.de>
2018-04-27 18:49:59 +0000

    EspressoBin: switched from xenial to bionic and renamed ubuntu-core* to ubuntu-base*

--- espresso-bin/ubuntu-base-02.sh
+++ espresso-bin/ubuntu-base-02.sh
@@ -4,23 +4,22 @@ set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 
-c_locale "en_GB.UTF-8" "de_DE.UTF-8"
-c_tzone "Europe/Berlin"
 c_hostname "ebin"
-c_apt_list "xenial"
-c_nameserver "8.8.8.8"
+c_apt_list "bionic"
+c_nameserver "1.1.1.1"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 
 r_pkg_upgrade
+echo Y | unminimize
 i_base
 i_extra
 i_kernel_espresso_bin
-c_if_lo
-c_if_dhcp "lan0"
-echo "  pre-up /sbin/ip link set eth0 up" >> /etc/network/interfaces.d/lan0
-c_ttyS "ttyMV0"
+c_if_netplan "lan0"
+netplan apply
 c_fw_utils "/dev/mtd1 0x0 0x00010000 0x1000 0x10"
+c_locale "en_GB.UTF-8" "de_DE.UTF-8"
+c_tzone "Europe/Berlin"
 c_user "ubuntu"
 
 apt-get clean
