725c406a7f74ed5da7c7f768e553282fc3610885
Uli Middelberg <uli@middelberg.de>
2018-04-27 19:10:59 +0000

    N1: switched from xenial to bionic and renamed ubuntu-core* to ubuntu-base*

--- odroid-n1/ubuntu-base-02.sh
+++ odroid-n1/ubuntu-base-02.sh
@@ -5,20 +5,18 @@ set -ex
 export DEBIAN_FRONTEND=noninteractive
 
 c_hostname "n1"
-#c_apt_list "bionic"
-c_apt_list "xenial"
-c_nameserver "8.8.8.8"
+c_apt_list "bionic"
+c_nameserver "1.1.1.1"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 
 r_pkg_upgrade
-#echo Y | unminimize
+echo Y | unminimize
 i_base
 i_extra
 i_kernel_odroid_n1_44
-c_if_lo
-c_if_dhcp "eth0"
-#systemctl enable getty@ttyFIQ0
+c_if_netplan "eth0"
+netplan apply
 c_fw_utils "/dev/mmcblk1 0x3c000 0x2000 0x200"
 c_locale "en_GB.UTF-8" "de_DE.UTF-8"
 c_tzone "Europe/Berlin"
