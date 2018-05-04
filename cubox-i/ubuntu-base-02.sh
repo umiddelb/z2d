0d48af0f7fb2d9a48f84c25aefc01cabfe95a823
Uli Middelberg <uli@middelberg.de>
2018-05-04 22:58:36 +0200

    Cubox-i: first release of bionic for armhf

--- cubox-i/ubuntu-base-02.sh
+++ cubox-i/ubuntu-base-02.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
-set -ex 
-
+set -ex
 . ./common-functions.sh
 
 export DEBIAN_FRONTEND=noninteractive
 
-c_locale "en_GB.UTF-8" "de_DE.UTF-8"
-c_tzone "Europe/Berlin"
 c_hostname "cbxi"
-c_apt_list "xenial"
-c_nameserver "8.8.8.8"
+c_apt_list "bionic"
+c_nameserver "1.1.1.1"
 
 dpkg-divert --local --rename --add /sbin/initctl; ln -s /bin/true /sbin/initctl
 
 r_pkg_upgrade
+echo Y | unminimize
 i_base
 i_extra
 i_kernel_cubox_i
-c_if_lo
-c_if_dhcp "eth0"
-c_ttyS "ttymxc0"
+c_if_netplan "eth0"
+netplan apply
 c_fw_utils "/dev/mmcblk0 0x60000 0x2000 0x2000"
+c_locale "en_GB.UTF-8" "de_DE.UTF-8"
+c_tzone "Europe/Berlin"
 c_user "ubuntu"
 
 apt-get clean
